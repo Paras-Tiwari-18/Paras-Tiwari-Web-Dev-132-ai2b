66071ba70227f70107eb233b8b596ed9a3c704cb
Your Name <parastiwari970@gmail.com>
2024-12-23 18:34:24 +0530

    Initial commit with project files

---
+++ init/data.js
@@ -0,0 +1,45 @@
+const sampleListings = [
+    {
+        title: "Choco Fudge",
+        description: "Badabite Ice Cream Candy",
+        image: {
+            url: "https://media.graphassets.com/XIcf0Bc5QRuK4EZyT3IC",
+        },
+    },
+    {
+        title: "American Nuts",
+        description: "Flingo Ice Cream Cone",
+        image: {
+            url: "https://media.graphassets.com/uzvEJdQH6BWMlNTJqU2g",
+        },
+    },
+    {
+        title: "Rajwadi",
+        description: "Rajwadi Ice Cream Candy",
+        image: {
+            url: "https://media.graphassets.com/6ywh7xmXTomu9tVmxhL7",
+        },
+    },
+    {
+        title: "Choco Crackle Flingo",
+        description: "Flingo Ice Cream Cone",
+        image: {
+            url: "https://media.graphassets.com/kht7P6R6Sp2nRtpSsicA",
+        },
+    },
+    {
+        title: "Choco Crackle",
+        description: "Gourmet Tubs",
+        image: {
+            url: "https://media.graphassets.com/hW0QUioQqaL0ORIjyQmW",
+        },
+    },
+    {
+        title: "Falooda",
+        description: "Gourmet Natural Tubs",
+        image: {
+            url: "https://media.graphassets.com/DA2UNPUJTcyBeXDnaNqn",
+        },
+    }
+];
+module.exports = { data: sampleListings };
